4128f82ef4803f6e0a699f79b563bd2dd73316a1
mak <mak@maks-MacBook-Pro.local>
2012-01-13 17:31:34 +0100

    added webserver

--- src/paraTwist.hs
+++ src/paraTwist.hs
@@ -19,23 +19,6 @@ import Control.Monad.Trans (liftIO)
 import ParaTwist.Types
 import ParaTwist.Warp
 
-dbsettings :: DatabaseSettings
-dbsettings = DatabaseSettings {
-        hostName          = "1.app.over9000.org",
-        portNumber        = 27017,
-        databaseName      = "adeven",
-        collectionName    = "rawRecords"
-        }
-
-adevenImpression :: AdevenImression
-adevenImpression = AdevenImression {
-        uid         = "1234567890abcdefghijklmnopqrstuvwxyz1234",
-        countryId   = "1a",
-        device      = "android",
-        campaignId  = "active",
-        sourceId    = "webseiteXYZ"
-        }
-
 warpSettings :: WarpSettings
 warpSettings = WarpSettings {
     warpPort = 3000
